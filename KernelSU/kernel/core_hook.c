69f28744acf0f0b4d65307f78a60e8095e139bf5
NCLnclNCL <126905141+NCLnclNCL@users.noreply.github.com>
2025-06-16 14:52:17 +0700

    Update core_hook.c

--- KernelSU/kernel/core_hook.c
+++ KernelSU/kernel/core_hook.c
@@ -943,11 +943,13 @@ static void ksu_try_umount(const char *mnt, bool check_mnt, int flags)
 
 	if (path.dentry != path.mnt->mnt_root) {
 		// it is not root mountpoint, maybe umounted by others already.
+		path_put(&path);
 		return;
 	}
 
 	// we are only interest in some specific mounts
 	if (check_mnt && !should_umount(&path)) {
+		path_put(&path);
 		return;
 	}
 #if defined(CONFIG_KSU_SUSFS_TRY_UMOUNT) && defined(CONFIG_KSU_SUSFS_ENABLE_LOG)
