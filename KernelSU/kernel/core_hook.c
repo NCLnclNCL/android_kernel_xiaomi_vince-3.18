ab801aa7c05a2a5db0695e8624545804c150a76e
NCLnclNCL <126905141+NCLnclNCL@users.noreply.github.com>
2025-06-16 15:02:56 +0700

    Update core_hook.c

--- KernelSU/kernel/core_hook.c
+++ KernelSU/kernel/core_hook.c
@@ -187,6 +187,8 @@ static void setup_groups(struct root_profile *profile, struct cred *cred)
 
 	groups_sort(group_info);
 	set_groups(cred, group_info);
+	put_group_info(group_info);
+
 }
 
 static void disable_seccomp(void)
@@ -211,19 +213,19 @@ void ksu_escape_to_root(void)
 {
 	struct cred *cred;
 
-	rcu_read_lock();
 
-	do {
-		cred = (struct cred *)__task_cred((current));
-		BUG_ON(!cred);
-	} while (!get_cred_rcu(cred));
-
-	if (cred->euid.val == 0) {
-		pr_warn("Already root, don't escape!\n");
-		rcu_read_unlock();
+	cred = prepare_creds();
+	if (!cred) {
+		pr_err("%s: failed to allocate new cred.\n", __func__);Add commentMore actions
 		return;
 	}
 
+
+	if (cred->euid.val == 0) {
+		pr_warn("Already root, don't escape!\n");
+		abort_creds(cred);
+		return;
+	}
 	struct root_profile *profile = ksu_get_root_profile(cred->uid.val);
 
 	cred->uid.val = profile->uid;
@@ -254,8 +256,16 @@ void ksu_escape_to_root(void)
 
 	setup_groups(profile, cred);
 	
-	rcu_read_unlock();
 
+	commit_creds(cred);
+
+	// FIXME: Maybe need to remove? The tests were fine.
+	// reference: https://elixir.bootlin.com/linux/v5.4/source/fs/exec.c#L1456
+	if (cred) {
+		pr_info("%s: NULL-ing creds after committing..\n", __func__);
+		cred = NULL;
+	}
+	
 	// Refer to kernel/seccomp.c: seccomp_set_mode_strict
 	// When disabling Seccomp, ensure that current->sighand->siglock is held during the operation.
 	spin_lock_irq(&current->sighand->siglock);
