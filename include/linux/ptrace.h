06851b431345130ec74050f48a665f8373e62155
NCLnclNCL <126905141+NCLnclNCL@users.noreply.github.com>
2025-06-28 15:32:50 +0700

    Update ptrace.h

--- include/linux/ptrace.h
+++ include/linux/ptrace.h
@@ -142,7 +142,7 @@ static inline bool ptrace_event_enabled(struct task_struct *task, int event)
 static inline void ptrace_event(int event, unsigned long message)
 {
 	if (unlikely(ptrace_event_enabled(current, event))) {
-		current->ptrace_message = message;
+		//current->ptrace_message = message;
 		ptrace_notify((event << 8) | SIGTRAP, message);
 	} else if (event == PTRACE_EVENT_EXEC) {
 		/* legacy EXEC report via SIGTRAP */
