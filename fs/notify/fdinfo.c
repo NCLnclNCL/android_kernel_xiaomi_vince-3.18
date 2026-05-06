3e64b42009bc1fd3a28a61a2f677d7d702f10a0c
NCLnclNCL <126905141+NCLnclNCL@users.noreply.github.com>
2026-05-06 17:25:01 +0700

    Update fdinfo.c

--- fs/notify/fdinfo.c
+++ fs/notify/fdinfo.c
@@ -89,6 +89,7 @@ static int inotify_fdinfo(struct seq_file *m, struct fsnotify_mark *mark)
 	inode_mark = container_of(mark, struct inotify_inode_mark, fsn_mark);
 	inode = igrab(mark->i.inode);
 	if (inode) {
+		u32 mask = mark->mask & IN_ALL_EVENTS;
 		ret = seq_printf(m, "inotify wd:%x ino:%lx sdev:%x "
 				 "mask:%x ignored_mask:%x ",
 				 inode_mark->wd, inode->i_ino,
